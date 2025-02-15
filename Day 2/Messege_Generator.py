62e358099df1b725531c7ddc7281b0f55eb2079c
Abdullah Al Noman <165512794+noman797@users.noreply.github.com>
2025-02-15 22:42:09 +0600

    Add files via upload

---
+++ Day 2/Messege_Generator.py
@@ -0,0 +1,15 @@
+#Welcome Message Generator
+
+#Step 1: Ask for user details
+name=input("What's your name?")
+hobby=input("What's your hobby?")
+color=input("What's your favorite color?")
+
+#Step 2: Generate a personalized welcome message
+print("\n--- Welcome Messege ---")
+print("Date: 2025-02-15 Time: 10:40 AM")
+print(f"Hello, {name}! 👋")
+print("Welcome to the world of Python Programming! 🐍")
+print(f"It's great to know that you love {hobby}! 🎉")
+print(f"Get ready to build something amazing today with Python! 🚀")
+
---
+++ Day 2/print&f_string.py
@@ -0,0 +1,13 @@
+print("Hello, Python World!")
+
+print("The number is:", 4)
+print("2+2=",2+2)
+
+name = "Noman"
+print("Hello, ", name)
+
+print(f"Hello, {name}! Welcome to Python World!") #f string
+
+userName=input("Enter your name: ")
+print(f"Hello, {userName}! Welcome to Python World!") #f string
+
