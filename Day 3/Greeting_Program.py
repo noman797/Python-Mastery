1440404a6d4287b03acecdacdc1d35d7a9c3779c
Abdullah Al Noman <165512794+noman797@users.noreply.github.com>
2025-02-17 23:23:34 +0600

    Add files via upload

---
+++ Day 3/Greeting_Program.py
@@ -0,0 +1,12 @@
+#Personalized greeting program
+
+#Step 1: Ask for user details
+name=input("What is your name?")
+age=int(input("How old are you?"))
+color=input("What is your favourite color?")
+
+#Step 2: Greet the user
+print("\n---- Personalized Greeting ----")
+print(f"Hello {name}!")
+print(f"You are {age} years old and your favourite color is {color}.")
+print("Your are now ready to start your Python adventure!🚀")
